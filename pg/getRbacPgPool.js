838d7c8dff457af2e3cf210fa37296daac2bb083
joehecn <leanbrown@live.cn>
2023-02-24 10:49:39 +0800

    v1.0.8

--- pg/getRbacPgPool.js
+++ pg/getRbacPgPool.js
@@ -1,29 +1,50 @@
 
-// import { POSTGRESQL_RBAC_POOL } from '../config/index.js'
 import pkgPG from 'pg'
 
 const { Pool } = pkgPG
 
 let _rbacPgPool = null
+
+const handleClientError = (err, postgresqlRbacPool) => {
+  console.log('----- GlobalUserManage: handleClientError', Date.now())
+  console.error(err)
+  _rbacPgPool = createPool(postgresqlRbacPool)
+}
+
+const createPool = postgresqlRbacPool => {
+  console.log('----- GlobalUserManage: createPool:', Date.now())
+  const rbacPgPool = new Pool(postgresqlRbacPool)
+
+  rbacPgPool.on('connect', client => {
+    client.on('error', (err) => {
+      handleClientError(err, postgresqlRbacPool)
+    })
+  })
+
+  rbacPgPool.on('error', err => {
+    console.log('----- GlobalUserManage: rbacPgPool error', Date.now())
+    console.error(err)
+    throw err
+  })
+
+  return rbacPgPool
+}
+
+const checkPool = () => {
+  if (!_rbacPgPool) return false
+  console.log('----- GlobalUserManage: checkPool true', Date.now())
+  console.log({
+    totalCount: _rbacPgPool.totalCount,
+    idleCount: _rbacPgPool.idleCount,
+    waitingCount: _rbacPgPool.waitingCount
+  })
+
+  return true
+}
+
 const getRbacPgPoll = postgresqlRbacPool => {
-  if (!_rbacPgPool) {
-    const rbacPgPool = new Pool(postgresqlRbacPool)
-
-    rbacPgPool.on('connect', client => {
-      client.on('error', err => {
-        console.error('----- rbacPgPool client connect error')
-        console.error(err)
-        process.exit(-1)
-      })
-    })
-
-    rbacPgPool.on('error', err => {
-      console.error('----- rbacPgPool error')
-      console.error(err)
-      process.exit(-1)
-    })
-
-    _rbacPgPool = rbacPgPool
+  if (!checkPool()) {
+    _rbacPgPool = createPool(postgresqlRbacPool)
   }
 
   return _rbacPgPool
--- pg/rbacQuery.js
+++ pg/rbacQuery.js
@@ -12,20 +12,31 @@
 //   }
 // }
 export const getQ = rbacPgPool => {
+  // const q = async query => {
+  //   // console.log({ rbacPgPool })
+  //   const client = await rbacPgPool.connect()
+  //   try {
+  //     const res = await client.query(query)
+  //     return res
+  //   } catch (err) {
+  //     console.log('------ q error:')
+  //     console.error(err)
+  //     throw err
+  //   } finally {
+  //     // Make sure to release the client before any error handling,
+  //     // just in case the error handling itself throws an error.
+  //     client.release()
+  //   }
+  // }
+
   const q = async query => {
-    // console.log({ rbacPgPool })
-    const client = await rbacPgPool.connect()
     try {
-      const res = await client.query(query)
+      const res = await rbacPgPool.query(query)
       return res
-    } catch (err) {
+    } catch (error) {
       console.log('------ q error:')
       console.error(err)
       throw err
-    } finally {
-      // Make sure to release the client before any error handling,
-      // just in case the error handling itself throws an error.
-      client.release()
     }
   }
 
